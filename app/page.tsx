054ad85ca91dda8b5453d716e4d0b384be7894de
edc <cadiz.edward@gmail.com>
2024-02-28 01:25:48 -0800

    initialize

---
+++ app/page.tsx
@@ -0,0 +1,35 @@
+import { getClient } from '@faustwp/experimental-app-router';
+import { gql } from '@apollo/client';
+import Link from 'next/link';
+
+export default async function Home() {
+  let client = await getClient();
+
+  const { data } = await client.query({
+    query: gql`
+      query GetPosts {
+        posts {
+          nodes {
+            id
+            title
+            uri
+            slug
+          }
+        }
+      }
+    `,
+  });
+
+  return (
+    <main>
+      <h2 className='m-20'>Posts</h2>
+      <ul>
+        {data.posts.nodes.map((post) => (
+          <li>
+            <Link href={`/${post.slug}`}>{post.title}</Link>
+          </li>
+        ))}
+      </ul>
+    </main>
+  );
+}
